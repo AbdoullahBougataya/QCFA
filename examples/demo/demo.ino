703455c41b821eacf4f19516a57e9ef1764d994a
AbdoullahBougataya <sience.story@gmail.com>
2023-07-27 14:34:30 +0100

    Add a button check for the drone

--- examples/demo/demo.ino
+++ examples/demo/demo.ino
@@ -6,6 +6,18 @@
 
 struct Quadcopter drone; /*get the drone parameters from the quadcopter structure*/
 
+int buttonPressed(int button)
+{
+  static int lastStates = 0;
+  int state = digitalRead(button);
+  if (state != ((lastStates >> button) & 1))
+  {
+    lastStates ^= 1 << button;
+    return state == HIGH;
+  }
+  return false;
+}
+
 void setup()
 {
   int motor1_esc_pin = 6;
@@ -31,19 +43,21 @@ void setup()
 
 void loop()
 {
-  up(drone, 2, 30);           /*up(Quadcopter parameter, height, speed)*/
-  levitate(drone, 1000);      /*levitate(Quadcopter parameter, levitation time)*/
-  forward(drone, 2, 30);      /*forward(Quadcopter parameter, distance, speed)*/
-  backward(drone, 2, 30);     /*backward(Quadcopter parameter, distance, speed)*/
-  rotate(drone, 90, 30);      /*rotate(Quadcopter parameter, angle, speed)*/
-  right(drone, 2, 30);        /*right(Quadcopter parameter, distance, speed)*/
-  left(drone, 2, 30);         /*left(Quadcopter parameter, distance, speed)*/
-  move_x(drone, 2, 30);       /*move_x(Quadcopter parameter, distance, speed)*/
-  move_y(drone, 2, 30);       /*move_y(Quadcopter parameter, distance, speed)*/
-  move_z(drone, 2, 30);       /*move_z(Quadcopter parameter, height, speed)*/
-  go_to(drone, -4, 4, 0, 30); /*goto(Quadcopter parameter, x coordinates, y coordinates, z coordinates, speed)*/
-  down(drone, 2);             /*down(Quadcopter parameter, height)*/
-  land(drone);                /*land(Quadcopter parameter)*/
-  stop(drone);                /*stop(Quadcopter parameter)*/
-  exit(0);                    /*exit from the loop*/
+  if (buttonPressed(2)) /*Check if a button is pressed on pin 2*/
+  {
+    up(drone, 2, 30);           /*up(Quadcopter parameter, height, speed)*/
+    levitate(drone, 1000);      /*levitate(Quadcopter parameter, levitation time)*/
+    forward(drone, 2, 30);      /*forward(Quadcopter parameter, distance, speed)*/
+    backward(drone, 2, 30);     /*backward(Quadcopter parameter, distance, speed)*/
+    rotate(drone, 90, 30);      /*rotate(Quadcopter parameter, angle, speed)*/
+    right(drone, 2, 30);        /*right(Quadcopter parameter, distance, speed)*/
+    left(drone, 2, 30);         /*left(Quadcopter parameter, distance, speed)*/
+    move_x(drone, 2, 30);       /*move_x(Quadcopter parameter, distance, speed)*/
+    move_y(drone, 2, 30);       /*move_y(Quadcopter parameter, distance, speed)*/
+    move_z(drone, 2, 30);       /*move_z(Quadcopter parameter, height, speed)*/
+    go_to(drone, -4, 4, 0, 30); /*goto(Quadcopter parameter, x coordinates, y coordinates, z coordinates, speed)*/
+    down(drone, 2);             /*down(Quadcopter parameter, height)*/
+    land(drone);                /*land(Quadcopter parameter)*/
+    stop(drone);                /*stop(Quadcopter parameter)*/
+  }
 }
